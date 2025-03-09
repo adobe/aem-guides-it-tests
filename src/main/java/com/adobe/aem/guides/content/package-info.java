b1c7217dd5e37a6e51a4cd25f697daf79c234182
pulgupta <pulgupta@adobe.com>
2025-03-09 14:05:28 +0530

    added package info classes

---
+++ src/main/java/com/adobe/aem/guides/content/package-info.java
@@ -0,0 +1,17 @@
+/*
+ * Copyright 2022 Adobe. All rights reserved.
+ * This file is licensed to you under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License. You may obtain a copy
+ * of the License at http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under
+ * the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR REPRESENTATIONS
+ * OF ANY KIND, either express or implied. See the License for the specific language
+ * governing permissions and limitations under the License.
+ */
+
+/**
+ * This package contains classes having map and topic content.
+ * The map and topic content is used to create a ditamap and dita topic in AEM.
+ */
+package com.adobe.aem.guides.content;
